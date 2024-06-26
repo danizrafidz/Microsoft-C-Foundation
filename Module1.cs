5399d8275a8c8fd1ea2851a7db367594df589ef5
Danzen <105960343+danizrafidz@users.noreply.github.com>
2024-06-26 10:01:44 +0700

    Create Module1.cs

---
+++ Module1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This is the first line.");
+	  Console.Write("This is ");
+	  Console.Write("the second ");
+	  Console.Write("line.");    
+    }
+  }
+}
